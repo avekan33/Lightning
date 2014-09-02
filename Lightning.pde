4e6869bc14ea70c19aa800a531582b68001943bd
avekan33 <avekan33@gmail.com>
2014-09-02 11:24:55 -0700

    index.html

--- Lightning.pde
+++ Lightning.pde
@@ -1,13 +1,60 @@
+int startX;
+int startY;
+int changeX;
+int changeY;
+int colorX;
+int fade;
+boolean control;
 void setup()
 {
-  size(300,300);
+	size(510,255);
+	background(0);
+	startX = 0;
+	startY = 127;
+	changeX = 0;
+	changeY = 0;
+	control = false;
+	frameRate(240);
+	fade = 0;
 }
+
 void draw()
 {
-
+	lightning();
 }
 void mousePressed()
 {
-
+	control = !control;
+}
+void lightning()
+{
+	if(control)
+	{
+		changeX = startX + int(random(10));
+		changeY = startY + int(random(-10,10));
+		if(startX <256)
+		{
+			colorX = startX;
+		}
+		else
+		{
+			colorX = 510 - startX;
+		}
+		stroke(0,colorX,startY);
+		line(startX,startY,changeX,changeY);
+		startX = changeX;
+		startY = changeY;
+		if(startX >510)
+		{
+			startX = 0;
+			startY = 127;
+			while(fade<121)
+			{	
+				fade++;
+			}
+			fill(0,fade);
+			rect(0,0,510,255);
+		}
+	}
 }
 
--- build-tmp/source/Lightning.java
+++ build-tmp/source/Lightning.java
@@ -14,19 +14,64 @@ import java.io.IOException;
 
 public class Lightning extends PApplet {
 
+int startX;
+int startY;
+int changeX;
+int changeY;
+int colorX;
+int fade;
+boolean control;
 public void setup()
 {
-  size(300,300);
-  noLoop();
+	size(510,255);
+	background(0);
+	startX = 0;
+	startY = 127;
+	changeX = 0;
+	changeY = 0;
+	control = false;
+	frameRate(240);
+	fade = 0;
 }
+
 public void draw()
 {
-  int x = (int)(Math.random() * 300);
-  ellipse(x,150,10,10);
+	lightning();
 }
 public void mousePressed()
 {
-  redraw();
+	control = !control;
+}
+public void lightning()
+{
+	if(control)
+	{
+		changeX = startX + PApplet.parseInt(random(10));
+		changeY = startY + PApplet.parseInt(random(-10,10));
+		if(startX <256)
+		{
+			colorX = startX;
+		}
+		else
+		{
+			colorX = 510 - startX;
+		}
+		stroke(0,colorX,startY);
+		line(startX,startY,changeX,changeY);
+		startX = changeX;
+		startY = changeY;
+		if(startX >510)
+		{
+			startX = 0;
+			startY = 127;
+			while(fade<121)
+			{	
+				fade++;
+			}
+			fill(0,fade);
+			rect(0,0,510,255);
+		}
+	}
 }
 
   static public void main(String[] passedArgs) {
